ffffbe4e1741f8b8949df0091e2882c34df9ae18
David Stein <david.stein@icahn.mssm.edu>
2024-07-09 11:38:18 -0400

    merge procedure

--- scripts/merge_output.py
+++ scripts/merge_output.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import sys
+import os
 
-on_the_fly = pd.read_csv(sys.argv[2])
-precomputed = pd.read_csv(sys.argv[1])
+on_the_fly = pd.DataFrame()
+precomputed = pd.DataFrame()
 
-out = pd.concat([precomputed, on_the_fly])
+if os.path.exists(sys.argv[1]):
+    precomputed = pd.read_csv(sys.argv[1])
+if os.path.exists(sys.argv[2]):
+    on_the_fly = pd.read_csv(sys.argv[2])
+
+out = pd.concat([precomputed, on_the_fly], ignore_index=True)
 out['CHROM'] = out['ID'].apply(lambda v: v.split('_')[0])
 out['POS'] = out['ID'].apply(lambda v: v.split('_')[1])
 out = out.sort_values(['CHROM', 'POS'])
